dbfe3632265ae22155ef4522f6690ed920bafbac
designeralabi <bdlkrmalabi@gmail.com>
2022-08-05 01:27:21 +0100

    added support for large screen size, and discard usage of eslint-plugin, using forwardRef properly

--- components/Results.jsx
+++ components/Results.jsx
@@ -4,7 +4,7 @@ import FlipMove from "react-flip-move";
 
 const Results = ({ results }) => {
   return (
-    <FlipMove  className=" px-5 my-18 sm:grid md:grid-cols-2 xl:grid-cols-3 ">
+    <FlipMove  className=" px-5 my-18 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:grid-cols-4">
       {results.map((result) => (
         <Thumbnail key={result.id} result={result} />
       ))}
